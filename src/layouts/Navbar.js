7c317568bd365e7899006a04d40bc3dde4748044
Junaid Choudhary <junaidchoudhary210@gmail.com>
2020-04-09 07:34:04 -0700

    removed extras and started actual thing

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,58 +1,7 @@
 import React, { Component } from 'react'
-import Link from 'gatsby-link'
-import logoLight from '../../static/assets/img/logo-light.png'
-import logoDark from '../../static/assets/img/logo-dark.png'
-import { StaticQuery, graphql } from 'gatsby';
-
-/* eslint-disable */
 
 export default function Navbar() {
   return (
-    <StaticQuery 
-      query={navlinkQuery}
-      render = {data => (
-        <nav className="navbar navbar-expand-lg navbar-dark">
-      <div className="container">
-
-        <div className="navbar-left">
-          <button className="navbar-toggler" type="button">☰</button>
-          <Link className="navbar-brand" to="/">
-            <img className="logo-dark" src={logoDark}/>
-            <img className="logo-light" src={logoLight} />
-          </Link>
-        </div>
-
-        <section className="navbar-mobile">
-          <nav className="nav nav-navbar ml-auto">
-            {data.site.siteMetadata.navBar.links.map(element => (
-              <a href={element.url} className="nav-link">{element.text}</a>
-            ))}
-          </nav>
-
-          <span className="navbar-divider"></span>
-
-          <div>
-            <Link className="btn btn-sm btn-round btn-primary ml-lg-4 mr-2" to="#">Download App</Link>
-          </div>
-        </section>
-
-      </div>
-    </nav>
-    )} />
+    <div>Navbar</div>
   )
 }
-
-const navlinkQuery = graphql`
-  query {
-      site {
-        siteMetadata {
-          navBar {
-            links {
-              text
-              url
-            }
-          }
-        }
-      }
-  }
-`
