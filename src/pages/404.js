943785ad0c4b31dc75c7df145f1b79fdf6ec5529
Junaid Choudhary <junaidchoudhary210@gmail.com>
2020-04-10 00:02:41 +0530

    added the NotFound page

---
+++ src/pages/404.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+export default function NotFound() {
+  return (
+    <main className="main-content text-center pb-lg-8">
+      <div className="container">
+        <h1 className="display-1 text-muted mb-7">Page Not Found</h1>
+        <p className="lead">
+          Seems you're looking for something that doesn't exist.
+        </p>
+        <br />
+        <button
+          className="btn btn-secondary w-150 mr-2"
+          type="button"
+          onClick={() => window.history.back()}
+        >
+          Go back
+        </button>
+        <a className="btn btn-secondary w-150" href="/">
+          Return Home
+        </a>
+      </div>
+    </main>
+  )
+}
